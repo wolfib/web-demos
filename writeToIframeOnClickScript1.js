21f33b7df8338ee39df5cbcef8523bfdb0ceaebd
Wolfgang Beyer <wolfi@chromium.org>
2020-11-25 11:11:23 +0100

    add script to pageWithResources

---
+++ writeToIframeOnClickScript1.js
@@ -0,0 +1,9 @@
+document.getElementById('button').onclick = () => {
+  const el = document.getElementById('frame');
+  console.log('element', el);
+  const doc = el.contentDocument;
+  console.log(doc);
+  doc.open();
+  doc.write('<h1>Written with document.write()</h1><img src="mickey.jpg" /><link rel="stylesheet" href="styles2.css">');
+  doc.close();
+}
