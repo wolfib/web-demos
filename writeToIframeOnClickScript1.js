271d6b65f0a1843253191ece8470e10f621c62d8
Wolfgang Beyer <wolfi@chromium.org>
2020-11-26 15:17:19 +0100

    format

--- writeToIframeOnClickScript1.js
+++ writeToIframeOnClickScript1.js
@@ -4,6 +4,10 @@ document.getElementById('button').onclick = () => {
   const doc = el.contentDocument;
   console.log(doc);
   doc.open();
-  doc.write('<h1>Written with document.write()</h1><img src="mickey.jpg" /><link rel="stylesheet" href="styles2.css">');
+  doc.write(`
+    <h1>Written with document.write()</h1>
+    <img src="mickey.jpg" />
+    <link rel="stylesheet" href="styles2.css">
+  `);
   doc.close();
 }
