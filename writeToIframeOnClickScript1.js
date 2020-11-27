b7cfcad31a584d4dde54e93585a8c233a934357e
Wolfgang Beyer <wolfi@chromium.org>
2020-11-27 15:25:27 +0100

    small revert

--- writeToIframeOnClickScript1.js
+++ writeToIframeOnClickScript1.js
@@ -3,17 +3,11 @@ document.getElementById('button').onclick = () => {
   console.log('element', el);
   let doc = el.contentDocument;
   console.log(doc);
-  // doc.open();
+  doc.open();
   doc.write(`
     <h1>1 Written with document.write()</h1>
     <img src="mickey.jpg" />
     <link rel="stylesheet" href="styles2.css">
   `);
-  doc = document.getElementById('frame').contentDocument;
-  doc.write(`
-  <h1>2 Written with document.write()</h1>
-  <img src="mickey.jpg" />
-  <link rel="stylesheet" href="styles2.css">
-`);
   doc.close();
 }
