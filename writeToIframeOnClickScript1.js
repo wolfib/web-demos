712dc53c9a57978bc910bd462e7b4bb4beeb2c21
Wolfgang Beyer <wolfi@chromium.org>
2020-11-27 15:08:03 +0100

    write twice 2

--- writeToIframeOnClickScript1.js
+++ writeToIframeOnClickScript1.js
@@ -1,7 +1,7 @@
 document.getElementById('button').onclick = () => {
   const el = document.getElementById('frame');
   console.log('element', el);
-  const doc = el.contentDocument;
+  let doc = el.contentDocument;
   console.log(doc);
   // doc.open();
   doc.write(`
@@ -9,6 +9,7 @@ document.getElementById('button').onclick = () => {
     <img src="mickey.jpg" />
     <link rel="stylesheet" href="styles2.css">
   `);
+  doc = document.getElementById('frame').contentDocument;
   doc.write(`
   <h1>2 Written with document.write()</h1>
   <img src="mickey.jpg" />
